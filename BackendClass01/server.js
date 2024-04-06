886c21873ede1d0711cc8925e126ada6b7fed4ff
PUNEET VAISHNAV <65516671+iamfutureflash@users.noreply.github.com>
2024-04-06 14:51:47 +0530

    learned about connection to mongodb

--- BackendClass01/server.js
+++ BackendClass01/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(5000, () => {
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.listen(3000, () => {
     console.log("Server Started at port 3000");
 });
 
@@ -9,7 +11,44 @@ app.get('/', (req, res) => {
     res.send("<h1>🚀🤘Hello Jee, kaise ho saare</h1>")
 })
 app.post('/api/cars', (req, res) => {
-    const { name, brand } = req.body();
-    console.log(brand,name);
-    res.send('Car submitted successfully')
+    const { name, brand } = req.body;
+    console.log(brand, name);
+    const data = {
+        name,
+        brand,
+        message: 'Car submitted successfully'
+    }
+    res.send(data);
+});
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/myDatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+    .then(() => { console.log("Connection Successful") })
+    .catch((error) => { console.log("Recieved and error ", error); });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
